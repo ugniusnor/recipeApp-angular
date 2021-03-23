3b1600f8464c064aff70f710dc3b9b2d11ac9ad0
ugniusnor <ugnn97@gmail.com>
2021-03-23 12:05:38 +0200

    init

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test rescipe',
+      'this is simple test recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+    ),
+    new Recipe(
+      'A test rescipe',
+      'this is simple test recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+    ),
+    new Recipe(
+      'A test rescipe',
+      'this is simple test recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/shoping-list/shoping-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../shared/ingridient.model';
+@Component({
+  selector: 'app-shoping-list',
+  templateUrl: './shoping-list.component.html',
+  styleUrls: ['./shoping-list.component.css'],
+})
+export class ShopingListComponent implements OnInit {
+  ingridients: Ingridient[] = [
+    new Ingridient('Apples', 5),
+    new Ingridient('Tomatoes', 2),
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
